5f3596bcf944f4c61df27fb20c35c2291ab43036
RodtigoWitkowski <RodtigoWitkowski>
2026-07-07 20:03:36 -0300

    finishd commenting inner workings of app.js

--- app.js
+++ app.js
@@ -50,28 +50,28 @@ function createTodoItem(todo, todoIndex){ //creates individual todo HTML element
                          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>
     ` //assigns html to todoListUL 
-    const deleteButton = todoListItem.querySelector(".delete-button");
-    deleteButton.addEventListener("click", ()=>{
+    const deleteButton = todoListItem.querySelector(".delete-button"); //selects the delet button element
+    deleteButton.addEventListener("click", ()=>{ //event listener (when button clicked execute deleTodoItem(todoIndex))
         deleteTodoItem(todoIndex);
     })
-    const checkbox = todoListItem.querySelector("input");
-    checkbox.addEventListener("change", ()=>{
-        allTodos[todoIndex].completed = checkbox.checked;
-        saveTodos();
+    const checkbox = todoListItem.querySelector("input"); //gets the first "input" element inside todoListItem
+    checkbox.addEventListener("change", ()=>{ //listens for when the checkbox changes state (its a boolean state so we can match with the todoObject property)
+        allTodos[todoIndex].completed = checkbox.checked; //when changed, todoObject "completed" property is assigned the checkbox boolean value
+        saveTodos(); //save todos in localStorage
     })
-    checkbox.checked = todo.completed;
-    return todoListItem;
+    checkbox.checked = todo.completed; //renders checkbox state
+    return todoListItem; //returned to be stored in todoItem back in updateTodoList()
 }
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
-    saveTodos();
-    updateTodoList();
+function deleteTodoItem(todoIndex){ //deletes todo with specific todoIndex (called by createTodoItem() if delete button is clicked)
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex); // Create a new array that keeps every todo except the one at todoIndex
+    saveTodos(); //save todos in localStorage
+    updateTodoList(); //renders updated todos
 }
-function saveTodos(){
-    const todosJson = JSON.stringify(allTodos);
-    localStorage.setItem("todos", todosJson);
+function saveTodos(){ //saves todos in localStorage
+    const todosJson = JSON.stringify(allTodos); //converts allTodos array into a json with strings
+    localStorage.setItem("todos", todosJson); //saves the JSON in local storage with key "todos" (not one key per todo, but one JSON for all)
 }
-function getTodos(){
-    const todos = localStorage.getItem("todos") || "[]";
-    return JSON.parse(todos);
+function getTodos(){ //retrieves JSON from localStorage
+    const todos = localStorage.getItem("todos") || "[]"; // gets Items (or if there isnt anything gets empty value)
+    return JSON.parse(todos); //converts JSON to object (array in this case) this is assigned to the global variable allTodos at the start of the code
 }
