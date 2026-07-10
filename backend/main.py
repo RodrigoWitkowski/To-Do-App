143c6221d6f44d18c18392662890f1c811c799f8
Wit <rodrigowitkowski@gmail.com>
2026-07-10 07:09:14 -0300

    Add .gitignore and update main.py for improved Todo ID handling

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -7,12 +8,12 @@ class TodoCreate(BaseModel):
     text: str
     completed: bool = False
 
-todo = []
+todos = []
 
 @app.post("/todos",status_code=201)
 async def create_todo(todo: TodoCreate):
-    todo_data = todo.model_dump()
-    todo_data["id"] = len(todos) + 1
+    todo_data = todo.model_dump() ##why model_dump()?
+    todo_data["id"] = str(uuid4())
 
     todos.append(todo_data)
     return todo_data
